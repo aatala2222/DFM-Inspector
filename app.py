843ed694514da3e52fb69db8a3a0c4bc724ec577
aatala2222 <aleatala@amazon.com>
2026-07-17 15:44:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_report_generator():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', processes={})
 
 @app.route('/health')
 def health():
